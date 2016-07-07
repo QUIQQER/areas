221aaa5e5571e113185a8eb0e48ef51707cc06e1
Henning Leutz <leutz@pcsg.de>
2016-07-07 13:17:15 +0200

    chore: Areas Utils

---
+++ src/QUI/ERP/Areas/Utils.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * This files contains QUI\ERP\Areas\Utils
+ */
+namespace QUI\ERP\Areas;
+
+use QUI;
+use QUI\Countries\Country;
+use QUI\Interfaces\Users\User;
+
+/**
+ * Class Utils
+ * Helper for area usage
+ *
+ * @package QUI\ERP\Areas
+ */
+class Utils
+{
+    /**
+     * Checks if the user in the area list
+     *
+     * @param User $User - user
+     * @param array $areas - list of areas or areas ids
+     * @return bool
+     */
+    public static function isUserInAreas(User $User, array $areas)
+    {
+        $Areas   = new Handler();
+        $Country = $User->getCountry();
+
+        foreach ($areas as $Area) {
+            if (!is_object($Area) || get_class($Area) != Area::class) {
+                try {
+                    $Area = $Areas->getChild($Area);
+                } catch (QUI\Exception $Exception) {
+                    continue;
+                }
+            }
+
+            /* @var $Area Area */
+            if ($Area->contains($Country)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Return the area where the country is in
+     *
+     * @param Country $Country
+     * @return bool|Area
+     */
+    public static function getAreaByCountry(Country $Country)
+    {
+        $Areas = new Handler();
+        $areas = $Areas->getChildren();
+
+        /* @var $Area Area */
+        foreach ($areas as $Area) {
+            if ($Area->contains($Country)) {
+                return $Area;
+            }
+        }
+
+        return false;
+    }
+}
